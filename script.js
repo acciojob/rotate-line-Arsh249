00e8b2f739a223fb3f92b9ec0a543bca57bbab5f
Arsh Ilahi <156427743+Arsh249@users.noreply.github.com>
2024-08-19 10:43:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+ let angle = 0;
+        const rotateLine = () => {
+            angle = (angle + 2) % 360; // Increment the angle by 2 degrees and ensure it stays within 0-359
+            document.getElementById('line').style.transform = `rotate(${angle}deg)`;
+        };
+
+        setInterval(rotateLine, 20);
